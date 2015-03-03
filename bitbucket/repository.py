462c1be3e8316c71ffecc8a579d782866ce15fac
robwilkerson <rob@robwilkerson.org>
2015-03-03 08:51:59 -0500

    Pull private repos for any accessible owner.
    Sometimes you need to pull data from repos that arne't owned by you, but
    rather by a team that you belong to.

--- bitbucket/repository.py
+++ bitbucket/repository.py
@@ -20,12 +20,13 @@ class Repository(object):
         self.bitbucket = bitbucket
         self.bitbucket.URLS.update(URLS)
 
-    def _get_files_in_dir(self, repo_slug=None, dir='/'):
+    def _get_files_in_dir(self, repo_slug=None, owner=None, dir='/'):
         repo_slug = repo_slug or self.bitbucket.repo_slug or ''
+        owner = owner or self.bitbucket.username
         dir = dir.lstrip('/')
         url = self.bitbucket.url(
             'GET_ARCHIVE',
-            username=self.bitbucket.username,
+            username=owner,
             repo_slug=repo_slug,
             format='src')
         dir_url = url + dir
@@ -34,7 +35,7 @@ class Repository(object):
             repo_tree = response[1]
             url = self.bitbucket.url(
                 'GET_ARCHIVE',
-                username=self.bitbucket.username,
+                username=owner,
                 repo_slug=repo_slug,
                 format='raw')
             # Download all files in dir
@@ -45,7 +46,7 @@ class Repository(object):
             # recursively download in dirs
             for directory in repo_tree['directories']:
                 dir_path = '/'.join((dir, directory))
-                self._get_files_in_dir(repo_slug=repo_slug, dir=dir_path)
+                self._get_files_in_dir(repo_slug=repo_slug, owner=owner, dir=dir_path)
 
     def public(self, username=None):
         """ Returns all public repositories from an user.
@@ -60,9 +61,10 @@ class Repository(object):
             pass
         return response
 
-    def all(self):
-        """ Return own repositories."""
-        url = self.bitbucket.url('GET_USER', username=self.bitbucket.username)
+    def all(self, owner=None):
+        """ Return all repositories for a given owner """
+        owner = owner or self.bitbucket.username
+        url = self.bitbucket.url('GET_USER', username=owner)
         response = self.bitbucket.dispatch('GET', url, auth=self.bitbucket.auth)
         try:
             return (response[0], response[1]['repositories'])
@@ -70,39 +72,43 @@ class Repository(object):
             pass
         return response
 
-    def get(self, repo_slug=None):
+    def get(self, repo_slug=None, owner=None):
         """ Get a single repository on Bitbucket and return it."""
         repo_slug = repo_slug or self.bitbucket.repo_slug or ''
-        url = self.bitbucket.url('GET_REPO', username=self.bitbucket.username, repo_slug=repo_slug)
+        owner = owner or self.bitbucket.username
+        url = self.bitbucket.url('GET_REPO', username=owner, repo_slug=repo_slug)
         return self.bitbucket.dispatch('GET', url, auth=self.bitbucket.auth)
 
     def create(self, repo_name, scm='git', private=True, **kwargs):
-        """ Creates a new repository on own Bitbucket account and return it."""
+        """ Creates a new repository on a Bitbucket account and return it."""
         url = self.bitbucket.url('CREATE_REPO')
         return self.bitbucket.dispatch('POST', url, auth=self.bitbucket.auth, name=repo_name, scm=scm, is_private=private, **kwargs)
 
-    def update(self, repo_slug=None, **kwargs):
-        """ Updates repository on own Bitbucket account and return it."""
+    def update(self, repo_slug=None, owner=None, **kwargs):
+        """ Updates repository on a Bitbucket account and return it."""
         repo_slug = repo_slug or self.bitbucket.repo_slug or ''
-        url = self.bitbucket.url('UPDATE_REPO', username=self.bitbucket.username, repo_slug=repo_slug)
+        owner = owner or self.bitbucket.username
+        url = self.bitbucket.url('UPDATE_REPO', username=owner, repo_slug=repo_slug)
         return self.bitbucket.dispatch('PUT', url, auth=self.bitbucket.auth, **kwargs)
 
-    def delete(self, repo_slug=None):
+    def delete(self, repo_slug=None, owner=None):
         """ Delete a repository on own Bitbucket account.
             Please use with caution as there is NO confimation and NO undo.
         """
         repo_slug = repo_slug or self.bitbucket.repo_slug or ''
-        url = self.bitbucket.url('DELETE_REPO', username=self.bitbucket.username, repo_slug=repo_slug)
+        owner = owner or self.bitbucket.username
+        url = self.bitbucket.url('DELETE_REPO', username=owner, repo_slug=repo_slug)
         return self.bitbucket.dispatch('DELETE', url, auth=self.bitbucket.auth)
 
-    def archive(self, repo_slug=None, format='zip', prefix=''):
+    def archive(self, repo_slug=None, owner=None, format='zip', prefix=''):
         """ Get one of your repositories and compress it as an archive.
             Return the path of the archive.
 
             format parameter is curently not supported.
         """
+        owner = owner or self.bitbucket.username
         prefix = '%s'.lstrip('/') % prefix
-        self._get_files_in_dir(repo_slug=repo_slug, dir='/')
+        self._get_files_in_dir(repo_slug=repo_slug, owner=owner, dir='/')
         if self.bitbucket.repo_tree:
             with NamedTemporaryFile(delete=False) as archive:
                 with ZipFile(archive, 'w') as zip_archive:
